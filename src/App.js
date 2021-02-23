1d0047114e9c15eb1212a4d0be432a72f385865a
arbaaz-96 <60695628+arbaaz-96@users.noreply.github.com>
2021-02-23 12:24:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';  
+import './App.css';  
+import Aggriddemo from './Aggriddemo'  
+function App() {  
+  return (  
+    <div className="App">  
+      <Aggriddemo/>  
+    </div>  
+  );  
+}  
+
+export default App;
